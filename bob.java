f69555cf29786635aa664032f3cfdac94a55c42e
Nickjoshuablock <61505392+Nickjoshuablock@users.noreply.github.com>
2022-02-04 13:01:48 -0800

    made bob files

--- bob.java
+++ bob.java
@@ -1,1 +1,5 @@
-
+public class bob {
+    public static void main(String[] args) {
+        System.out.println("Hello Bob!");
+    }
+}
--- bobtest.java
+++ bobtest.java
@@ -1,1 +1,12 @@
+import static org.junit.Assert.*;
 
+import java.beans.Transient;
+
+import org.junit.*;
+
+public class bobtest{
+    @Test
+    public void add() {
+        assertEquals(3, 1+1);
+    }
+}
